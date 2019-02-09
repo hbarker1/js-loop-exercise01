b01e94720ef5624154d2448eae9a694f47f50709
hannah barker <hannah.m.barker@gmail.com>
2019-02-09 17:36:49 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# js-loop-exercise01
---
+++ loops-01.js
@@ -0,0 +1,28 @@
+for (let i = 1; i <=10; i++) {
+        console.log(i);
+
+    }
+
+for (let i = 10; i >= 1; i--) {
+        console.log(i);
+    }
+
+    let i = 1;
+    while (i <= 10) {
+        console.log(i);
+        i++;
+
+    }
+    
+    i = 1;
+    do  {
+        console.log(i);
+        i++;
+    
+    } while (i <= 10);
+
+
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    for (const numbers of numbers) {
+        console.log (numbers);
+    }
